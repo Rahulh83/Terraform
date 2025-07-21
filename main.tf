1859cac0cc774bf294304ca7f7c5d0109c4af1cc
unknown <rahulharsh261@outlook.com>
2025-07-21 14:30:06 +0530

    new added rg1

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rh" {
+    name = "rharsh-rg"
+    location = "East US"
+  
+}
---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+    features {}
+    subscription_id = "ba3dd1c9-2ade-4648-a13f-65581e2a2874"
+}
